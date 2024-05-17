f52eefbd4cdbf99e168a0baaf98560fd2831d6fe
RSP_linye <linyejoe2@gmail.com>
2024-05-17 23:27:09 +0800

    CICD v2

---
+++ build/web-mobile/cocos-js/meshopt_decoder.wasm-43855155.js
@@ -0,0 +1,11 @@
+System.register([], (function (exports, module) {
+	'use strict';
+	return {
+		execute: (function () {
+
+			var meshopt_decoder_wasm = exports('default', 'assets/meshopt_decoder.wasm-12c0a404.wasm'); /* asset-hash:bedd883e */
+
+		})
+	};
+}));
+//# sourceMappingURL=meshopt_decoder.wasm-43855155.js.map
