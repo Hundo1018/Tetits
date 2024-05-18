7d5917fa7797d0f09ede9e07c8071ab504a1627b
linyejoe2 <linyejoe2@gmail.com>
2024-05-18 18:11:59 +0000

    來自 GitHub Actions 的自動部署。

---
+++ cocos-js/spine-9a8528df.js
@@ -0,0 +1,11 @@
+System.register([], (function (exports, module) {
+	'use strict';
+	return {
+		execute: (function () {
+
+			var spine = exports('default', 'assets/spine-59f406dc.wasm'); /* asset-hash:78ca8b3f */
+
+		})
+	};
+}));
+//# sourceMappingURL=spine-9a8528df.js.map
