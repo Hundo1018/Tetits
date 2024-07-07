3094ce85f7c1583c3668b8db83147d558e789378
Yue <yue806161@gmail.com>
2024-07-07 14:57:34 +0800

    場景切換
    補寫主選單控制

--- assets/Systems/MenuController.ts
+++ assets/Systems/MenuController.ts
@@ -1,45 +1,58 @@
-import { _decorator, Button, Component, Node,  } from 'cc';
+import { _decorator, Button, Component, director, Node } from 'cc';
 const { ccclass, property, menu } = _decorator;
 
 @ccclass('MenuController')
-@menu('Sence-MainMenu')
+@menu('Scene-MainMenu')
 export class MenuController extends Component {
+    // page nodes
+    @property(Node) private newGamePage?: Node;
+    @property(Node) private cgPage?: Node;
+    @property(Node) private setPage?: Node;
+    @property(Node) private mainPage?: Node;
 
-    @property([Node])
-    newGamePage = null;
+    // main page buttons
+    @property(Button) private newGameBTN?: Button;
+    @property(Button) private cgBTN?: Button;
+    @property(Button) private setBTN?: Button;
+    @property(Button) private exitBTN?: Button;
 
-    @property([Node])
-    cgGalleryPage = null;
+    // pages back buttons
+    @property([Button]) private backBTN: Button[] = [];
 
-    @property([Node])
-    settingPage = null;
-
-    @property([Node])
-    mainPage = null;
-
-    @property([Button])
-    newGameBTN = null;
-
-    @property([Button])
-    cgGalleryBTN = null;
-
-    @property([Button])
-    settingBTN = null;
-
-    @property([Button])
-    exitGameBTN = null;
+    // new game page button
+    @property(Button) private startBTN?: Button;
 
     start() {
+        // main page
+        this.setupButton(this.newGameBTN, () => this._showPage(this.newGamePage));
+        this.setupButton(this.setBTN, () => this._showPage(this.setPage));
+        this.setupButton(this.cgBTN, () => this._showPage(this.cgPage));
+        this.setupButton(this.exitBTN, this.onQuitGame);
+        this.backBTN.forEach((btn) =>
+            this.setupButton(btn, () => this._showPage(this.mainPage))
+        );
 
+        // new game page
+        this.setupButton(this.startBTN, () => {
+            director.loadScene('scene-dialogue');
+        });
     }
 
-    protected onLoad(): void {
-        this.newGameBTN.on('')
+    setupButton(button: Button | undefined, callback: () => void) {
+        button?.node.on('click', callback, this);
     }
 
-    update(deltaTime: number) {
-        
+    private _showPage(page?: Node) {
+        if (page) {
+            this.newGamePage!.active = false;
+            this.cgPage!.active = false;
+            this.setPage!.active = false;
+            this.mainPage!.active = false;
+            page.active = true;
+        }
+    }
+
+    onQuitGame() {
+        console.log('退出遊戲');
     }
 }
-
-
